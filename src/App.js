3994918b21609d3a04c18a881ac207cff3943c15
jcuhnoio <junochoi2003@gmail.com>
2024-05-06 01:19:51 -0400

    Add multiple directions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
+/* eslint-disable no-undef */
+
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  APIProvider,
+  Map,
+  useMapsLibrary,
+  useMap,
+} from "@vis.gl/react-google-maps";
+import { useState, useEffect } from "react";
 
 function App() {
+  const position = { lat: 42.29, lng: -71.26 };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: "100vh" }}>
+        <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+          <Map
+            defaultCenter={position}
+            defaultZoom={12}
+            mapId={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+            fullscreenControl={false}
+          >
+            <Directions />
+          </Map>
+        </APIProvider>
+      </div>
     </div>
   );
+}
+
+function Directions() {
+  const map = useMap();
+  const routesLibrary = useMapsLibrary("routes");
+  const [directionsService, setDirectionsService] = useState();
+  const [directionsRenderer, setDirectionsRenderer] = useState();
+  const [routes, setRoutes] = useState([]);
+
+  const origin = "Olin College of Engineering";
+  const destinations = [
+    "Babson College",
+    "Wellesley College",
+    "Music Go Round Natick",
+    "Trader Joe's Needham",
+  ];
+
+  useEffect(() => {
+    if (!routesLibrary || !map) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+  }, [routesLibrary, map]);
+
+  useEffect(() => {
+    if (!directionsService || !directionsRenderer) return;
+    const routesArray = [];
+    for (let i = 0; i < destinations.length; i++) {
+      directionsService
+        .route({
+          origin: origin,
+          destination: destinations[i],
+          travelMode: google.maps.TravelMode.DRIVING,
+          provideRouteAlternatives: false,
+        })
+        .then((response) => {
+          setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+          directionsRenderer.setDirections(response);
+          routesArray.push(response.routes);
+        });
+      setRoutes(routesArray);
+    }
+  }, [directionsService, directionsRenderer]);
+
+  console.log(routes);
+  return null;
 }
 
 export default App;
